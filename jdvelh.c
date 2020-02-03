9caa3a314b43aaa4ee05c57cc57a147ae41fe665
AlexandreJoriteETPA <alexandre.jorite@gmail.com>
2020-02-03 15:10:21 +0100

    Update jdvelh.c

--- jdvelh.c
+++ jdvelh.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 struct Chemin{
-	char nomchemin[30];
-	int direction;
+    char nomchemin[30];
+    int direction;
 };
 
 typedef struct Chemin chemin;
@@ -12,23 +12,25 @@ int main(){
     
     chemin gauche = {"Route du désespoir",1};
     chemin droite = {"Route de la lumière",2};
-	int choix ;
-	int cheminG1 = 0;
-	int cheminD1 = 0;
+    int choix ;
+    int cheminG1 = 0;
+    int cheminD1 = 0;
 
 printf("Vous arriver devant un panneau direction quel chemin prendrez-vous! \n");
 
-printf("[1] Route du désespoir \n [2] Route de la lumière");
+printf("[1] Route du désespoir \n[2] Route de la lumière");
     scanf("%d", &choix);
 
     if (choix == 1){
-    	printf(" [1] %s \n", gauche.nomchemin, gauche.direction);
-    		printf("Bonne chance à travers cette route de danger obscure \n");
+        printf(" [1] %s \n", gauche.nomchemin, gauche.direction);
+            printf("Bonne chance à travers cette route de danger obscure! \n");
+            printf("Vous avez vaincue cette route maudite! \n");
     }
 
     if (choix == 2){
-    	printf(" [2] %s \n", droite.nomchemin, droite.direction);
-			printf("Rien ne vous arrivera sur se chemin profiter de la vue \n");
+        printf(" [2] %s \n", droite.nomchemin, droite.direction);
+            printf("Rien ne vous arrivera sur ce chemin profiter de la vue! \n");
+            printf("Vous ète arriver au bout de ce paisible chemin ! \n");
     }
 
     return 0;
